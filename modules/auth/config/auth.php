405a6400df222a4f182c04ed14819b93f31600a9
d3head <hello@oleg.so>
2014-04-09 18:31:13 +0700

    Index fix

--- modules/auth/config/auth.php
+++ modules/auth/config/auth.php
@@ -11,7 +11,8 @@ return array(
 
 	// Username/password combinations for the Auth File driver
 	'users' => array(
-		// 'admin' => 'b3154acf3a344170077d11bdb5fff31532f679a1919e716a02',
+		 //'admin' => 'b3154acf3a344170077d11bdb5fff31532f679a1919e716a02',
+		'admin'	=> 'b867e6f1b3d9763967ab81643560f55f5193c95a6a57e7a3d70bace9580e8ec9'
 	),
 
 );
