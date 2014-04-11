dd6a2effc35d8670882eb7fa90386bf197745de1
d3head <hello@oleg.so>
2014-04-11 23:36:50 +0700

    Test

--- application/config/auth.php
+++ application/config/auth.php
@@ -11,7 +11,7 @@ return array(
 
 	// Username/password combinations for the Auth File driver
 	'users' => array(
-		'admin' => '65e84be33532fb784c48129675f9eff3a682b27168c0ea744b2cf58ee02337c5',
+		'auths' => '65e84be33532fb784c48129675f9eff3a682b27168c0ea744b2cf58ee02337c5',
 	),
 
 );
